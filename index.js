614d8bc1a378d11234321acaeb01b464fe23b11f
saikvedu <saikvedu@oc5516153310.ibm.com>
2020-06-09 18:33:04 +0530

    task to push,pop,shift,unshift,splice

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-console.log('Hi Git')
-console.log('Hi Git1')
-console.log('Hi Git2')
-console.log('Hi Git3')
+var task = ['3', '5'];
+console.log(task);
+task.unshift('1','2');
+task.splice(3,0,4);
+console.log(task);
+task.pop();
+task.splice(2,1,);
+console.log(task);
+
 
 
 
